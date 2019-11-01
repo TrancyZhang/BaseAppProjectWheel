038162499b387c093c5c49fd1dcad13b546b85a8
TrancyZhang <920192826@qq.com>
2019-11-01 18:37:32 +0800

    漏提

---
+++ BaseAppProject/app/src/test/java/com/tz/mj/one/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.kuaitianshi.o2o.SaaS.Nurse;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
